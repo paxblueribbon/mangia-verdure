94f7c897e1ad8284e656d75e616962439e8aaa7b
Paxana <Veganbikepunk@gmail.com>
2021-02-02 11:20:32 -0800

    Added card layout, beginning of local db obj

--- js/scripts.js
+++ js/scripts.js
@@ -21,23 +21,39 @@ $(document).ready(function() {
         };
     };
 
-    var states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
-        'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
-        'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
-        'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
-        'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
-        'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
-        'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
-        'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
-        'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
-    ];
+    var localDb = {
+       apple: {
+           0: ["allspice", "cinnamon", "cloves", "ginger", "maple syrup", "orange"],
+           1: ["almonds", "cinnnamon", "rosemary"],
+           2: ["caramel", "nuts"],
+           3: ["cloves", "cranberries", "oranges"],
+           4: [""],
+           5: [""]
+       },
+       black_beans: {
+
+       },
+       coffee: {}
+    }
+
+        $('#ing1').on('typeahead:selected', function(evt, item) {
+            // do what you want with the item here
+            $(function() {
+                $(".row .col6 .card .card-body").each(function() {
+                  $(this).html("Found this one");
+                });
+              });
+            console.log(item)
+
+        })
+        
         $('#ing1').typeahead({
             hint: true,
             highlight: true,
             minLength: 1
         },
             {
-                name: 'states',
-                source: substringMatcher(states)
+                name: 'keys',
+                source: substringMatcher(Object.keys(localDb))
             });
 });
